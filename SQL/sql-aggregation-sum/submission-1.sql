6b5b4f2215208bb7b1f7d50fff6ca67bdfbcf3dd
Himanshu Tamrakar <himanshu.tamrakar@outlook.com>
2026-06-06 11:11:19 +0530

    Update: sql-aggregation-sum - submission-1

--- SQL/sql-aggregation-sum/submission-1.sql
+++ SQL/sql-aggregation-sum/submission-1.sql
@@ -1,17 +1,20 @@
-CREATE TABLE students (
-    id INTEGER PRIMARY KEY,
-    name TEXT
+CREATE TABLE products (
+    id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    name TEXT,
+    price INTEGER
 );
 
-INSERT INTO students (id, name)
-  VALUES (1, 'Alice'),
-         (2, 'Bob'),
-         (3, 'Charlie');
+INSERT INTO products (name, price) VALUES
+('Apple', 5),
+('Banana', 7),
+('Gift Card', 10),
+('Laptop', 1000),
+('Phone', 500),
+('Pen', 2);
 -- Do not modify above this line. --
 
-TRUNCATE TABLE students;
+SELECT SUM(price) as total_discount_price FROM products WHERE price <= 10;
 
 
 
--- Do not modify below this line. --
-SELECT * FROM students;
+
