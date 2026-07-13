26a087e06eef89cb8a93e8766f9813d2295966f0
Himanshu Tamrakar <himanshu.tamrakar@outlook.com>
2026-07-13 15:28:23 +0530

    Update: sql-delete-constraints - submission-3

--- SQL/sql-delete-constraints/submission-3.sql
+++ SQL/sql-delete-constraints/submission-3.sql
@@ -1,21 +1,31 @@
-CREATE TABLE bank_accounts (
-    id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-    name TEXT,
-    balance INTEGER,
-    last_transaction_date DATE
+CREATE TABLE classes (
+    id INTEGER PRIMARY KEY,
+    name TEXT
 );
 
-INSERT INTO bank_accounts (name, balance, last_transaction_date) VALUES
-    ('Alice', 1000, '2023-11-30'),
-    ('Bob', -100, '2023-12-31'),
-    ('Charlie', 2000, '2024-01-03'),
-    ('David', -500, '2024-01-04'),
-    ('Eve', 1500, '2024-01-05');
+CREATE TABLE students (
+    id INTEGER PRIMARY KEY,
+    name TEXT
+);
+
+CREATE TABLE enrollments (
+    id INTEGER PRIMARY KEY,
+    student_id INTEGER REFERENCES students(id),
+    class_id INTEGER REFERENCES classes(id)
+);
+
+INSERT INTO classes (id, name) VALUES (1, 'Math');
+INSERT INTO students (id, name) VALUES (1, 'Alice');
+INSERT INTO enrollments (id, student_id, class_id) VALUES (1, 1, 1);
 -- Do not modify above this line. --
 
 
+DELETE FROM enrollments;
+
+DELETE FROM classes;
+
+DELETE FROM students;
 
 
-DELETE FROM bank_accounts
-WHERE balance < 0 AND last_transaction_date < '2024-01-01'
-RETURNING id, name;
+-- Do not modify below this line. --
+SELECT * FROM enrollments;
