7e3475b32e603316d01e6987e9d14fc6535589d9
Himanshu Tamrakar <himanshu.tamrakar@outlook.com>
2026-07-24 16:13:25 +0530

    Add: sql-game-play-analysis-ii - submission-0

---
+++ SQL/sql-game-play-analysis-ii/submission-0.sql
@@ -0,0 +1,17 @@
+-- Write your query below
+SELECT player_id, device_id
+FROM (
+    SELECT player_id, device_id,
+    RANK() OVER(PARTITION BY player_id ORDER BY event_date) AS rnk
+    FROM activity
+) t
+WHERE rnk = 1;
+
+
+SELECT player_id, device_id
+FROM activity 
+WHERE (player_id, event_date) IN (
+    SELECT player_id, MIN(event_date)
+    FROM activity
+    GROUP BY player_id
+);
