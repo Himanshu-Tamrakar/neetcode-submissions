a3710c358aafa68d65573f4d7487ccac8f3fdc5d
Himanshu Tamrakar <himanshu.tamrakar@outlook.com>
2026-07-24 16:01:47 +0530

    Add: sql-find-the-team-size - submission-0

---
+++ SQL/sql-find-the-team-size/submission-0.sql
@@ -0,0 +1,16 @@
+-- Write your query below
+WITH team AS(
+    SELECT team_id, COUNT(*) AS team_size
+    FROM employee
+    GROUP BY team_id
+)
+
+SELECT e.employee_id, ts.team_size
+FROM employee e
+JOIN team ts ON e.team_id = ts.team_id;
+
+-- team_id | team_size
+-- 8       | 3
+-- 7       | 1
+-- 9       | 2
+
