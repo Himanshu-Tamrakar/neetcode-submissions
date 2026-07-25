c16126baeff36cde553025c64a1d968fd70e53fc
Himanshu Tamrakar <himanshu.tamrakar@outlook.com>
2026-07-25 10:56:30 +0530

    Add: sql-the-most-recent-three-orders - submission-0

---
+++ SQL/sql-the-most-recent-three-orders/submission-0.sql
@@ -0,0 +1,21 @@
+WITH most_recent AS(
+    SELECT c.name AS customer_name,
+            c.customer_id,
+            o.order_id,
+            o.order_date,
+            ROW_NUMBER() OVER(PARTITION BY o.customer_id ORDER BY o.order_date DESC) AS rn
+    FROM orders o
+    JOIN customers c ON o.customer_id = c.customer_id
+)
+
+SELECT customer_name, customer_id, order_id, order_date
+FROM most_recent
+WHERE rn <= 3
+ORDER BY 
+    customer_name ASC,
+    customer_id ASC,
+    order_date DESC;
+
+
+
+
