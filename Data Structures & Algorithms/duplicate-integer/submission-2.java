8055d7ad8feb125307aa8f42f76e26be5952b68e
Himanshu Tamrakar <himanshu.tamrakar@outlook.com>
2026-08-23 15:11:01 +0530

    Add: duplicate-integer - submission-2

---
+++ Data Structures & Algorithms/duplicate-integer/submission-2.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+        HashSet<Integer> st = new HashSet<>();
+        for (int num: nums) {
+            if (st.contains(num)) {
+                return true;
+            } else {
+                st.add(num);
+            }
+        }
+
+        return false;
+    }
+
+    // public boolean hasDuplicate(int[] nums) {
+    //     HashSet<Integer> st = new HashSet<>();
+    //     for (int num: nums) {
+    //         st.add(num);
+    //     }
+
+    //     return st.size() != nums.length;
+    // }
+}
