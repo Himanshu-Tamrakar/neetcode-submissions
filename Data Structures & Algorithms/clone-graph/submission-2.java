bbf59038ad74f7381a68d1668665a9cc67e6f446
Himanshu Tamrakar <himanshu.tamrakar@outlook.com>
2026-08-25 16:47:48 +0530

    Add: clone-graph - submission-2

---
+++ Data Structures & Algorithms/clone-graph/submission-2.java
@@ -0,0 +1,61 @@
+/*
+Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> neighbors;
+    public Node() {
+        val = 0;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val) {
+        val = _val;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val, ArrayList<Node> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+}
+*/
+
+class Solution {
+    public Node cloneGraph(Node node) {
+        HashMap<Node, Node> cloneMapping = new HashMap<>();
+        return dfs(node, cloneMapping);
+    }
+
+    private Node dfs(Node node, HashMap<Node, Node> cloneMapping) {
+        if (node == null) return node;
+
+        if (cloneMapping.containsKey(node)) {
+            return cloneMapping.get(node);
+        }
+        
+        Node clone = new Node(node.val);
+        cloneMapping.put(node, clone);
+        
+        for (Node neighbor: node.neighbors) {
+            clone.neighbors.add(dfs(neighbor, cloneMapping));
+        }
+
+        return clone;
+    }
+
+    // private Node dfs(Node node, HashMap<Node, Node> cloneMapping) {
+    //     if (node == null) return node;
+        
+    //     Node clone = new Node(node.val);
+    //     cloneMapping.put(node, clone);
+        
+    //     for (Node neighbor: node.neighbors) {
+    //         if (cloneMapping.containsKey(neighbor)) {
+    //             Node cloneNode = cloneMapping.get(neighbor);
+    //             clone.neighbors.add(cloneNode);
+    //         } else {
+    //             clone.neighbors.add(dfs(neighbor, cloneMapping));
+    //         }
+    //     }
+
+    //     return clone;
+    // }
+}
