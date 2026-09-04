6ffe236dafb099f154c35d08a98f2fdbc4cfefde
Himanshu Tamrakar <himanshu.tamrakar@outlook.com>
2026-09-04 22:56:29 +0530

    Add: find-smallest-common-element-in-all-rows - submission-0

---
+++ Data Structures & Algorithms/find-smallest-common-element-in-all-rows/submission-0.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int smallestCommonElement(int[][] mat) {
+        
+        for (int target: mat[0]) {
+            
+            boolean found = true;
+            for (int i = 1; i < mat.length; i++) {
+                if (rank(mat[i], 0, mat[i].length - 1, target) == -1) {
+                    found = false;
+                    break;
+                }
+            }
+
+            if (found) return target;
+
+        }
+
+        return -1;
+
+    }
+
+    private int rank(int[] arr, int lo, int hi, int target) {
+        
+        while (lo <= hi) {
+            int mid = lo + (hi - lo) / 2;
+            if (arr[mid] < target) lo = mid + 1;
+            else if (arr[mid] > target) hi = mid - 1;
+            else return mid;
+        }
+
+        return -1;
+    }
+}
