075a69f208dd1e0da0d5a7be70bf8cc82e6a0279
Himanshu Tamrakar <himanshu.tamrakar@outlook.com>
2026-09-05 13:00:41 +0530

    Add: one-edit-distance - submission-3

---
+++ Data Structures & Algorithms/one-edit-distance/submission-3.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean isOneEditDistance(String s, String t) {
+        int ns = s.length();
+        int nt = t.length();
+
+        if (s.length() > t.length()) {
+            return isOneEditDistance(t, s);
+        }
+
+        if (t.length() - s.length() > 1) {
+            return false;
+        }
+
+        for (int i = 0; i < ns; i++) {
+            if (s.charAt(i) != t.charAt(i)) {
+                if (ns == nt) {
+                    return s.substring(i + 1).equals(t.substring(i + 1));
+                } else {
+                    return s.substring(i).equals(t.substring(i + 1));
+                }
+            }
+        }
+
+        return (ns + 1) == nt;
+    }
+}
