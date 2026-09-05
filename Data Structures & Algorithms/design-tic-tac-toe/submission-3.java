5d34ea2b57fcbb671dcd89aa964d1039ee50d7fc
Himanshu Tamrakar <himanshu.tamrakar@outlook.com>
2026-09-05 21:50:49 +0530

    Add: design-tic-tac-toe - submission-3

---
+++ Data Structures & Algorithms/design-tic-tac-toe/submission-3.java
@@ -0,0 +1,68 @@
+class TicTacToe {
+    private int[][] board;
+    public TicTacToe(int n) {
+        board = new int[n][n];
+    }
+    
+    public int move(int row, int col, int player) {
+        int n = board.length;
+        board[row][col] = player;
+        if (checkRow(board, row, player) || 
+            checkCol(board, col, player) || 
+            (row == col && checkDiagonal(board, player)) || 
+            (col == n - row - 1 && checkAntiDiagonal(board, player)) ) {
+                return player;
+            }
+
+        return 0;
+    }
+
+    private boolean checkRow(int[][] board, int row, int player) {
+
+        for (int col = 0; col < board.length; col++) {
+            if (board[row][col] == 0 || board[row][col] != player) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private boolean checkCol(int[][] board, int col, int player) {
+        for (int row = 0; row < board.length; row++) {
+            if (board[row][col] == 0 || board[row][col] != player) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private boolean checkDiagonal(int[][] board, int player) {
+        int n = board.length;
+        for (int row = 0; row < board.length; row++) {
+            // check top left to bottom right 
+            if (board[row][row] == 0 || board[row][row] != player) {
+                return false;
+            } 
+        }
+        return true;
+    }
+
+    private boolean checkAntiDiagonal(int[][] board, int player) {
+        int n = board.length;
+        for (int row = 0; row < board.length; row++) {
+            // check top left to bottom right 
+            if (board[row][n - row - 1] == 0 || board[row][n - row - 1] != player) {
+                return false;
+            } 
+        }
+        return true;
+    }
+}
+
+/**
+ * Your TicTacToe object will be instantiated and called as such:
+ * TicTacToe obj = new TicTacToe(n);
+ * int param_1 = obj.move(row,col,player);
+ */
