21a4fa8572796b26a0f17b841807c112d26f0fd2
Himanshu Tamrakar <himanshu.tamrakar@outlook.com>
2026-09-06 17:45:32 +0530

    Add: wiggle-sort - submission-1

---
+++ Data Structures & Algorithms/wiggle-sort/submission-1.java
@@ -0,0 +1,35 @@
+class Solution {
+    public void wiggleSort(int[] nums) {
+        Arrays.sort(nums);
+        for (int i = 1; i < nums.length - 1; i += 2) {
+            int temp = nums[i];
+            nums[i] = nums[i + 1];
+            nums[i + 1] = temp;
+        }
+    }
+
+    // public void wiggleSort(int[] nums) {
+    //     int i = 0; 
+    //     int j = nums.length - 1;
+    //     int idx = 1;
+    //     int[] res = new int[nums.length];
+    //     Arrays.sort(nums);
+
+    //     while (i < j) {
+    //         if (odd(idx)) {
+    //             res[idx++] = nums[j--];
+    //         } else {
+    //             res[idx++] = nums[i++];
+    //         }
+    //     }
+    //     res[0] = nums[i];
+    //     for (int k = 0; k < nums.length; k++) {
+    //         nums[k] = res[k];
+    //     }
+        
+    // }
+
+    // private boolean odd(int i) {
+    //     return i % 2 == 0 ? false : true;
+    // }
+}
